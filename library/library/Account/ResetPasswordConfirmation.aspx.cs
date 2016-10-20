2365a565ff4155f039b71d60886f984f1951e236
akashdesai95 <akashdesai2007@gmail.com>
2016-10-20 10:15:17 +0530

    deleted

--- library/library/Account/ResetPasswordConfirmation.aspx.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Web.UI;
-
-namespace library.Account
-{
-    public partial class ResetPasswordConfirmation : Page
-    {
-    }
-}
--- library/library/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="library.Global" Language="C#" %>
